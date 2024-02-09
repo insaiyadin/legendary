39dcffc2334bc4afa4add6b163727bc47a50ae4e
Adrian Kierenkiewicz <adrian@kierenkiewicz.com>
2024-02-09 08:28:54 +0100

    testing volumes and claims

--- backend/src/helpers/log-file.ts
+++ backend/src/helpers/log-file.ts
@@ -1,11 +1,20 @@
 import { writeFile, rm } from "fs/promises";
+import { existsSync, mkdirSync } from "fs";
+
+const logsFileDir = "logs";
 
 const logFilePath = process.env.LOG_FILE_NAME as string;
 
 export const createLogFile = async () => {
+  if (!existsSync(logsFileDir)) {
+    mkdirSync(logsFileDir);
+  }
   await writeFile(logFilePath, "", { flag: "w" });
 };
 
 export const deleteLogFile = async () => {
+  if (!existsSync(logsFileDir)) {
+    return;
+  }
   await rm(logFilePath);
 };
